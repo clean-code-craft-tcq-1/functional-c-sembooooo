e4ac72b3a40ea6befc0d980acf751517531125ea
Sana Srikar (RBEI/ESP-IS14) <Srikar.Sana@in.bosch.com>
2021-03-02 17:32:51 +0530

    first commit

--- checker.c
+++ checker.c
@@ -1,18 +1,101 @@
 #include <stdio.h>
 #include <assert.h>
+#include "checker.h"
+
+#define TRUE 1
+#define FALSE 0
+
+int IsValueOutofLowerRangeThreshold(float value , float LowerThreshold)
+{
+  return (value < LowerThreshold);
+}
+
+int IsValueOutofHigherRangeThreshold(float value , float HigherThreshold)
+{
+  return (value > HigherThreshold);
+}
+
+char *Failuretype_str[Failuretype_TotalNumber] =
+{
+  "Out of Lower Range ",
+  "Out of Higher Range"
+};
+
+TemparatureControlData_t TemparatureControlData = {
+  0.0,            
+  45.0,           
+  "Temparature" 
+  };
+
+int IsTempStable(float Temparature)
+{
+  int isTemparaturestable = TRUE;
+  if(IsValueOutofLowerRangeThreshold(Temparature,TemparatureControlData.LowerOutOfRangeThreshold))
+  {
+    printf("%s %s\n",
+          TemparatureControlData.Identifier,
+          Failuretype_str[Failuretype_OutOfLowerRange]);
+          isTemparaturestable = FALSE;
+  }
+  
+  if(IsValueOutofHigherRangeThreshold(Temparature,TemparatureControlData.HigherOutOfRangeThreshold))
+  {
+        printf("%s %s\n",
+          TemparatureControlData.Identifier, Failuretype_str[Failuretype_OutOfHigherRange]);
+          isTemparaturestable = FALSE;
+  }
+     return isTemparaturestable;
+}
+
+StateOfChargeDeterminationControlData_t StateOfChargeDeterminationControlData ={
+  20.0,             
+  80.0,             
+  "StateOfCharge" 
+  };
+
+int IsStateOfChargeDetermination(float soc)
+{
+  int IsStateOfChargeDeterminationok = TRUE;
+    if(IsValueOutofLowerRangeThreshold(soc,StateOfChargeDeterminationControlData.LowerOutOfRangeThreshold))
+  {
+    printf("%s %s\n",
+          StateOfChargeDeterminationControlData.Identifier,
+          Failuretype_str[Failuretype_OutOfLowerRange]);
+          IsStateOfChargeDeterminationok = FALSE;
+  }
+  
+  if(IsValueOutofHigherRangeThreshold(soc,StateOfChargeDeterminationControlData.HigherOutOfRangeThreshold))
+  {
+        printf("%s %s\n",
+          StateOfChargeDeterminationControlData.Identifier,
+          Failuretype_str[Failuretype_OutOfHigherRange]);
+        IsStateOfChargeDeterminationok = FALSE;
+  }
+  return IsStateOfChargeDeterminationok;
+}
+
+ChargeRateControlData_t ChargeRateControlData = {0.8,"ChargeRate"};
+
+int IschargeRateOk(float chargeRate)
+{
+  int IsChargeRateok = TRUE;
+  if(IsValueOutofHigherRangeThreshold(chargeRate,ChargeRateControlData.HigherOutOfRangeThreshold))
+  {
+    printf("%s %s\n",
+          ChargeRateControlData.Identifier,
+          Failuretype_str[Failuretype_OutOfHigherRange]);
+          IsChargeRateok = FALSE;
+  }
+  return IsChargeRateok;
+}
+
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
-  return 1;
+  int istemparatureok, issocok, ischargerateok;
+  istemparatureok = IsTempStable(temperature);
+  issocok = IsStateOfChargeDetermination(soc);
+  ischargerateok = IschargeRateOk(chargeRate);
+  return ( istemparatureok && issocok && ischargerateok);
 }
 
 int main() {
