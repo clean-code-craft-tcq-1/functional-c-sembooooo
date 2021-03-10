28648f2da10fc0d5e3ae010b97db2428d1cbcc67
Sana Srikar (RBEI/ESP-IS14) <Srikar.Sana@in.bosch.com>
2021-03-10 08:45:02 +0530

    another patch up

--- BatteryHealthMonitoring.c
+++ BatteryHealthMonitoring.c
@@ -60,13 +60,11 @@ int batteryIsOk()
 }
 
 int main() {
-  #if 0
   FeedBatteryParameterValues(param_value[0]);
+  assert(batteryIsOk());
+  FeedBatteryParameterValues(param_value[2]);
   assert(batteryIsOk());
   FeedBatteryParameterValues(param_value[1]);
   assert(!batteryIsOk());
-  #endif
-  FeedBatteryParameterValues(param_value[2]);
-  assert(batteryIsOk());
   return 0;
 }
